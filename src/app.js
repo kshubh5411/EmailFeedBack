56ae36d3783c93972730cb5ab514b88f4f074512
shubhamk541 <shubhamk541@gmail.com>
2018-12-27 01:30:31 +0530

    initial commit2

---
+++ src/app.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import AppRouter, {history} from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import {login, logout} from './actions/auth';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+import LoadingPage from './components/LoadingPage';
+import axios from 'axios';
+
+const store = configureStore();
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+let hasRendered = false;
+const renderApp = () => {
+    if(!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }
+}
+
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
+
+const onAuthStateChanged = () => {
+    axios({
+        method:'get',
+        url:'/users/me',
+        headers: {'x-auth': localStorage.getItem('x-auth')}
+      }).then((response) => {
+        store.dispatch(login({
+            uid: response.data._id,
+            fullName: response.data.fullName,
+            email: response.data.email
+        }))
+        renderApp();
+        if(history.location.pathname === '/') {
+            history.push('/dashboard');
+        }
+      }).catch((e) => {
+        console.log(e);
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
+      });
+}
+
+onAuthStateChanged();
+export default onAuthStateChanged;
