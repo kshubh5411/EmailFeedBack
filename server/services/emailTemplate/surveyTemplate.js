87c38d271c3292e0222e39d346dfc1aea4940f4a
shubhamk541 <shubhamk541@gmail.com>
2019-02-13 19:43:01 +0530

    Styling the Email Page

--- server/services/emailTemplate/surveyTemplate.js
+++ server/services/emailTemplate/surveyTemplate.js
@@ -1,9 +1,14 @@
+
 module.exports = survey =>
 {
    return   `
        <html>
-         <div>
-             <p>${survey.body}</p>
+         <div class="container" style="background: orange; text-align: center;" >
+             <h3>Do You Fascinated With our Community</h3>
+              <p>Please Reply for the Following Question</p>
+              <p>${survey.body}</p>
+            <div><a href="http://localhost:3000/">Yes</a></div>
+            <div><a href="http://localhost:3000/">No</a></div>
           </div>
        </html>
    `
