666cd613aa5759104be3b5e2181dc7b5381dd4f0
shubhamk541 <shubhamk541@gmail.com>
2019-02-17 02:03:09 +0530

    Add Action button in dash board

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,7 +3,7 @@ import { Router, Route, Switch} from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 import createHistory from 'history/createBrowserHistory'
-import LandingPage from '../components/LandingPage';
+import Dashboard from '../components/Dashboard';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
 
@@ -14,7 +14,8 @@ const AppRouter = () => (
         <div>
             <Switch>
                 <PublicRoute path="/" component={LoginPage} exact={true}/>
-                <PrivateRoute path="/landingpage" component={LandingPage} /> 
+                <PrivateRoute path="/surveys" component={Dashboard} /> 
+                
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -10,7 +10,7 @@ export const PublicRoute = ({
     <div>
         <Route {...rest} component={(props) => (
             isAuthenticated ? (
-                <Redirect to="/landingpage" />
+                <Redirect to="/surveys" />
             ) : (
                 <Component {...props}/>
             )
